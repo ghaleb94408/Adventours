2dbd67e337184ad2a231c1fe3bbdb4debf6b8a21
Ghaleb Murabyeh <ghaleb.murapyeh@gmail.com>
2023-03-24 01:23:35 +0200

    finnish tour template and route and add login template

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -4,15 +4,27 @@ const catchAsync = require('../utility/catchAsync');
 exports.getOverview = catchAsync(async (req, res) => {
   // 1) Get tours data from DB
   const tours = await Tour.find();
-  // 2) Build Template
   // 3) Render Template
   res.status(200).render('overview', {
     title: 'All Tours',
     tours,
   });
 });
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async (req, res) => {
+  // 1) Get tour data from DB
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
+  });
+  // 2) Render Template
   res.status(200).render('tour', {
-    title: 'The Forest Hiker',
+    title: tour.name,
+    tour,
+  });
+});
+exports.login = (req, res) => {
+  console.log('log in ....');
+  res.status(200).render('login', {
+    title: 'Log in',
   });
 };
--- routers/viewsRoutes.js
+++ routers/viewsRoutes.js
@@ -3,5 +3,6 @@ const viewController = require('../controllers/viewsController');
 
 const router = express.Router();
 router.get('/', viewController.getOverview);
-router.get('/tour', viewController.getTour);
+router.get('/tours/:slug', viewController.getTour);
+router.get('/login', viewController.login);
 module.exports = router;
